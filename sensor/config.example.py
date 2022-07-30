890f4e4e0f9c235befe95abd1876b362c1a88100
Bailey Kasin <bailey@gingertechnology.net>
2022-07-30 08:50:33 +0000

    example config

---
+++ sensor/config.example.py
@@ -0,0 +1,7 @@
+SECRET_KEY="production api key"
+TWILIO_SID=''
+TWILIO_AUTH=''
+TWILIO_MESSAGING_ID=''
+TWILIO_CONTACT_NUM='phone number with +(areacode) at the start. no spaces or -'
+ALERT_TEMP = 90.0
+ALERT_HUMIDITY = 60.0
